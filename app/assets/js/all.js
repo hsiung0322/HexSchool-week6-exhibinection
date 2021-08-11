2b49c2bf2e6bda671d4d56ed48f8b9bd774dca3b
熊正奇 <as7196528@gmail.com>
2021-08-11 15:36:22 +0800

    first

---
+++ app/assets/js/all.js
@@ -0,0 +1,26 @@
+$(function() {
+  console.log('Hello Bootstrap5');
+
+  // $('.nav-link').click(function(){
+  //   $('.nav .nav-link').removeClass('active');
+  //   $(this).addClass('active');
+  // });
+
+  // swiper
+  var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    breakpoints: {
+      // when window width is >= 768px
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 30
+      }
+    },
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+  });
+
+});
